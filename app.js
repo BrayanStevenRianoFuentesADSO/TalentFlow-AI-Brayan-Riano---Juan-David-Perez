06f9420ad6f40ecfe6224e11489f40c012495dcf
BrayanStevenRianoFuentesADSO <brayanstevenriano@gmail.com>
2026-08-12 11:18:13 -0500

    feat: agregar formulario, html, css y js para conectar al webhook n8n

---
+++ app.js
@@ -0,0 +1,38 @@
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", async (event) => {
+
+    event.preventDefault();
+
+    const webhookURL = "https://unworldly-unbalance-nautical.ngrok-free.dev/webhook-test/postulacion";
+
+    const formData = new FormData(formulario);
+
+    try {
+
+        const respuesta = await fetch(webhookURL, {
+            method: "POST",
+            body: formData
+        });
+
+        if (!respuesta.ok) {
+            throw new Error("Error al enviar los datos");
+        }
+
+        const resultado = await respuesta.text();
+
+        console.log("Respuesta de n8n:", resultado);
+
+        alert("Datos enviados correctamente");
+
+        formulario.reset();
+
+    } catch (error) {
+
+        console.error("Error:", error);
+
+        alert("No se pudieron enviar los datos");
+
+    }
+
+});
